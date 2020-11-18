a70e32ad5e2672386490ff4501bdcda27013be59
syacooms <syacooms@gmail.com>
2020-11-18 14:54:12 +0900

    2. landingPage

--- 2. boilerplate-mern-stack-master/client/src/components/Config.js
+++ 2. boilerplate-mern-stack-master/client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'e8664e38b6755a202e9c5d2300ed9dbf'
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
--- 2. boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ 2. boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,61 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_KEY, API_URL, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
+
+
+    const [Movies, setMovies] = useState([])
+    const  [MainMovieImage, setMainMovieImage] = useState(null)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        
+        //영화들을 가져오는 fetch
+        fetch(endpoint)
+        // result
+        .then(response => response.json())
+        .then(response => {
+
+            console.log(response)
+            setMovies([...response.results])
+            setMainMovieImage(response.results[0])
+            console.log(response.results[0].backdrop_path)
+        });
+       
+    }, [])
+
     return (
         <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '100%', margin: '0'}}>
+            
+            {/* Main Image */}
+
+            {/* backdroppath를 가져오기전에 렌더링을 했다.
+             && 표현은 값이 있으면 가져오라??  */}
+        {MainMovieImage &&
+            <MainImage 
+            image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+            title={MainMovieImage.original_title}
+            text={MainMovieImage.overview} 
+            />
+        }
+            
+            <div style={{width: '85%', margin: '1rem auto' }}>
+
+                <h2>Movies by latest</h2>
+                {/* <img src='http://image.tmdb.org/t/p/w1280/gnf4Cb2rms69QbCnGFJyqwBWsxv.jpg' /> */}
+                <hr />
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
+
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <button>Load More</button>
+            </div>
+
+
+        </div>
+
         </>
     )
 }
